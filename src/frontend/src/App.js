c08edcdad5b50a8761c69f18eec5308e9dd09fa1
Michael Maggiore <michaelpmaggiore@gmail.com>
2024-05-08 14:55:44 -0600

    deleted uncessary files

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate, Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TodoList from './TodoList';
-import TodoDetail from './TodoDetail';
-import NewTodo from './NewTodo';
-import ProjectList from './ProjectList'; // Component to display all projects
-import NewProject from './NewProject'; // Component to create a new project
-import ProjectDetail from './ProjectDetail'; // Component to display todos in a project
-import Project from './ProjectList';
 import './App.css';
 import HomePage from './HomePage';
 import UserContext from './UserContext';
@@ -42,16 +35,7 @@ function App() {
         <LogoutButton />
 
         <Routes>
-          <Route path="/project" element={<ProjectList />} />
-          <Route path="/project/new" element={<NewProject />} />
-          <Route path="/project/:projectId" element={<ProjectDetail />} />
-          
-          <Route path="/project/:projectId/todo" element={<TodoList />} />
-          <Route path="/project/:projectId/todo/new" element={<NewTodo />} />
-          <Route path="/project/:projectId/todo/:todoId" element={<TodoDetail />} /> 
-        
-
-          <Route exact path="/" element={<HomePage />} component={Project} />
+          <Route exact path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} /> 
           <Route path="/register" element={<Register />} />
           <Route path="/schedule/:userId" element={<Schedule />} />
